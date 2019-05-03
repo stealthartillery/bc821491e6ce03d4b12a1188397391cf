26468ad6518be832a320e70434e6bb437ab3e22a
stealthartillery <stealth.artillery@gmail.com>
2019-05-03 10:49:07 +0700

    enlite backlite change

--- be.lanterlite.com/application/controllers/User.php
+++ be.lanterlite.com/application/controllers/User.php
@@ -14,7 +14,8 @@ class User extends CI_Controller{
 
 	public function index() {
 		$data = json_decode(file_get_contents("php://input"), true);
-			header('Content-Type: application/json');
+			// header('Content-Type: application/json');
+			header("HTTP/1.1 200 OK");
 			echo 'asd';
 		// $this->L->printJson($data);
 		if ($data !== null && $this->L->is_exist_json_key($data, "Data") and $this->L->is_exist_json_key($data, 'LSSK')) {
