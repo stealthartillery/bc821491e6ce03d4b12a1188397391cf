c6e865e5e16078abe367dc0fdbc3d28b179076e0
stealthartillery <stealth.artillery@gmail.com>
2019-06-28 11:00:44 +0700

    add perl file

--- perl.lanterlite.com/hello.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-use cPanelUserConfig;
-# The above line is perl execution path in xampp
-# The below line tells the browser, that this script will send html content.
-# If you miss this line then it will show "malformed header from script" error.
-print "Content-type: text/html\n\n";
-
-print "Hello world."
