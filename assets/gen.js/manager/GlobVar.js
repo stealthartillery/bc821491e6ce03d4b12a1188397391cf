89b236c41500210a6869c90b8ddea498a4d13362
stealthartillery <stealth.artillery@gmail.com>
2019-09-27 10:35:11 +0700

    update lanterlite

--- assets/gen.js/manager/GlobVar.js
+++ assets/gen.js/manager/GlobVar.js
@@ -6,7 +6,7 @@ function GlobVar() {
 	this.init = init
 	function init() {
 		console.log(window.location.pathname)
-		LGen.JsonMan.read(BASE_URL+'assets/gen.obj/globvar', function(obj) {
+		LGen.JsonMan.read(BASE_URL+'assets/gen.obj/globvar.lgen', function(obj) {
 			keys = LGen.JsonMan.get_keys(obj);
 			for (var i=0; i<keys.length; i++) {
 				LGen.GlobVar[keys[i]] = obj[keys[i]]
