67c1cc98d46ca0abcd8ba2a85e91894db5af1d93
stealthartillery <stealth.artillery@gmail.com>
2019-09-27 10:09:41 +0700

    update lanterlite

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<!doctype html><html><head>
+<!doctype html>
+<head>
 	<title>Lanterlite</title>
 	<meta name="title" content="Lanterlite">
 	<meta name="description" content="Lanterlite is a technology company that focuses on the development of Islam. Vision to be the leading IT-based organization in expanding Islam throughout the world. Its mission rests on spreading the teachings of Islam to every human being so that it is easiest to get knowledge about Islam from clear references. Lanterlite moves with its own concepts and techniques in carrying out its mission with the ultimate goal that leads to the welfare of humanity through Islamic science as a guide to human life.">
@@ -12,7 +13,7 @@
 	<link rel="icon" type="image/gif" href="https://www.lanterlite.com/app/gen.base.fe/icon.gif">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
-</head><body></body>
+</head>
+<body></body>
 <script type="text/javascript" src="https://www.lanterlite.com/app/gate.js"></script>
 </html>
