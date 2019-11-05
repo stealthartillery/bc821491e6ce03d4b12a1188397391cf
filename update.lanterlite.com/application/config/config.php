a403b6f8d1901ca3c9084becc5fdae2fc3cd7ca6
stealthartillery <stealth.artillery@gmail.com>
2019-11-05 13:57:42 +0700

    update update.lanterlite.com

--- update.lanterlite.com/application/config/config.php
+++ update.lanterlite.com/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = HOST;
+$config['base_url'] = BASE_URL;
 // $config['base_url'] = 'http://192.168.1.5:80/lanterlite.com/';
 
 /*
