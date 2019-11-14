294906456173cc4689c629e61884bd4e43c90bd4
stealthartillery <stealth.artillery@gmail.com>
2019-11-14 08:28:40 +0700

    add store

--- be.citizen.lanterlite.com/application/controllers/Gate.php
+++ be.citizen.lanterlite.com/application/controllers/Gate.php
@@ -1,8 +1,12 @@
 <?php if (!defined('BASEPATH')) exit('Site protected by Lanterlite Defender.');
 
-/* v 1.0.0 */
+/* v 1.0.2 */
 
 init();
+function get_app() {
+	$res = json_decode(file_get_contents(HOME_DIR."app/app.lgen"), true);
+	return $res;
+}
 
 class Gate extends CI_Controller{
 
@@ -11,33 +15,28 @@ class Gate extends CI_Controller{
 	}
 
 	public function index() {
-		include BASE_DIR . 'lantergen.php';
+		include BASE_DIR . 'assets/gen_base_be/lantergen.php';
 		include HOME_DIR . 'app/lanterapp.php';
 		$data = json_decode(file_get_contents("php://input"), true);
 		if (isset($_POST[DATA]))
 			$data = $_POST;
 
-		if (isset($data)) {
+		if (isset($data))
 			$asd = LGen('White')->get($data[DATA]);
-			if (array_key_exists('f', $asd))
-				$asd['func'] = $asd['f'];
-			if (array_key_exists('o', $asd))
-				$asd['json'] = $asd['o'];
-			$func = urldecode($asd['func']);
-			$json = $asd['json'];
-			eval('$result = '.$func.'($json);');
-			echo LGen('Black')->get($result);
-		}
-		else if (isset($_GET["f"])) {
+		else if (isset($_GET["f"]))
 			$asd = LGen('White')->get($_GET["f"]);
-			if (array_key_exists('f', $asd))
-				$asd['func'] = $asd['f'];
-			if (array_key_exists('o', $asd))
-				$asd['json'] = $asd['o'];
-			$func = urldecode($asd['func']);
-			$json = $asd['json'];
-			eval('$result = '.$func.'($json);');
-			echo LGen('Black')->get($result);
-		}
+		else 
+			exit();
+		if (array_key_exists('f', $asd))
+			$asd['func'] = $asd['f'];
+		if (array_key_exists('o', $asd))
+			$asd['json'] = $asd['o'];
+		$func = urldecode($asd['func']);
+		$json = $asd['json'];
+		header("HTTP/1.1 200 OK");
+		header('Content-type: text/plain');
+		eval('$result = '.$func.'($json);');
+		echo LGen('Black')->get($result);
+		exit();
 	}
 }
