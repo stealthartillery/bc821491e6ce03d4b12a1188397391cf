622d06b5bb71ef746c57099e7d5e71ecac90c700
stealthartillery <stealth.artillery@gmail.com>
2019-11-20 10:28:35 +0700

    update lantersite

--- store.lanterlite.com/app/page/page_order_success.js
+++ store.lanterlite.com/app/page/page_order_success.js
@@ -31,11 +31,11 @@ function OrderSuccess () {
 		var page = page_order_success
 		LGen.set_button(page.screen.querySelector('.home_btn'))
 		LGen.set_button_click(page.screen.querySelector('.home_btn'), function(_this) {
-			if (typeof window.parent != "undefined"){
-				var someIframe = window.parent.document.getElementById('ch_iframe');
-				lprint(someIframe)
-				someIframe.parentNode.removeChild(window.parent.document.getElementById('ch_iframe'));
-			}
+			// if (typeof window.parent != "undefined"){
+			// 	var someIframe = window.parent.document.getElementById('ch_iframe');
+			// 	lprint(someIframe)
+			// 	someIframe.parentNode.removeChild(window.parent.document.getElementById('ch_iframe'));
+			// }
 			LGen.Page.change(page.previous_page)
 		})
 	}
