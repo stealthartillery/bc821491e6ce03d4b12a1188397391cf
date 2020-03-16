d886aeb294972959280fa1d3368db78a26f65474
ifandhanip <ifandhanip@gmail.com>
2020-03-16 21:41:09 +0700

    add exp php

---
+++ exp/test.php
@@ -0,0 +1,19 @@
+<?php
+
+
+	echo exec('php --version');
+	// echo exec('php test_asd.php');
+	// echo exec('git --version');
+	// echo exec('php server.php');
+
+	// function read($file_path) { 
+	// 	if (file_exists($file_path)) { 
+	// 		$string = file_get_contents($file_path); 
+	// 		return json_decode($string, true); 
+	// 	} 
+	// 	else
+	// 		return LGen('GlobVar')->failed;
+	// }
+
+
+?>
