8c67087646a9ad766052472bfacacbd9016ef9c1
ifandhanip <ifandhanip@gmail.com>
2020-03-17 17:10:07 +0700

    add exp php

--- exp/websocket/cli/server.php
+++ exp/websocket/cli/server.php
@@ -27,7 +27,7 @@ require __DIR__ . '/../src/Application/Application.php';
 require __DIR__ . '/../src/Application/DemoApplication.php';
 require __DIR__ . '/../src/Application/StatusApplication.php';
 
-$server = new \Bloatless\WebSocket\Server('www.lanterlite.com', 443);
+$server = new \Bloatless\WebSocket\Server('www.lanterlite.com', 1000);
 
 // server settings:
 $server->setMaxClients(100);
